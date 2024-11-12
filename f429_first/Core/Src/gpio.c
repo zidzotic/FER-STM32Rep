4aac66e12fdbe09da82b930c37f8a21b042f6590
zvonimir_idzotic <zvonimir.idzotic@gmail.com>
2024-11-12 01:41:30 +0100

    added first stm32f429 blinky

---
+++ f429_first/Core/Src/gpio.c
@@ -0,0 +1,77 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOG, USER_LED1_Pin|USER_LED2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PGPin PGPin */
+  GPIO_InitStruct.Pin = USER_LED1_Pin|USER_LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+void gpio_led_state ( uint8_t LED_ID , uint8_t state ) {
+	GPIO_PinState pinState ;
+	pinState = ( state == 1) ? GPIO_PIN_SET : GPIO_PIN_RESET ;
+	switch ( LED_ID ) {
+		case LED1_USER_ID :
+			HAL_GPIO_WritePin (USER_LED1_GPIO_Port , USER_LED1_Pin , pinState ) ;
+			break ;
+		case LED2_USER_ID :
+			HAL_GPIO_WritePin (USER_LED2_GPIO_Port , USER_LED2_Pin , pinState ) ;
+			break ;
+	}
+}
+
+/* USER CODE END 2 */
